6640fcc6488b1fe582abacb634ebe8b82da8ddc8
Patryk <dzynio233@o2.pl>
2024-10-02 17:12:20 +0200

    Create categories.php
    categories translation for polish language

---
+++ translations/pl/categories.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'access_controlled_by_private_ancestor' => 'Kategoria nadrzędna :category jest ustawiona jako prywatna i kontroluje dostęp do tej kategorii.',
+    'actions' => 'Akcje kategorii',
+    'category' => 'Kategoria|Kategorie',
+    'confirm_nonempty_delete' => 'Tak, chcę trwale usunąć tę kategorię i wszystko, co się w niej znajduje',
+    'create' => 'Stwórz kategorię',
+    'created' => 'Kategoria została utworzona',
+    'deleted' => 'Kategoria została usunięta|Kategorie zostały usunięte',
+    'disable_threads' => 'Wyłącz wątki',
+    'edit' => 'Edytuj kategorię',
+    'enable_threads' => 'Włącz wątki',
+    'make_private' => 'Ustaw jako prywatne',
+    'make_public' => 'Ustaw jako publiczne',
+    'mark_read' => 'Oznacz wątki w tej kategorii jako odczytane',
+    'marked_read' => 'Nowe/zaktualizowane wątki w :category zostały oznaczone jako przeczytane',
+    'parent' => 'Kategoria nadrzędna',
+    'restored' => 'Kategoria przywrócona|Kategorie przywrócone',
+    'subcategories' => 'Podkategorie',
+    'threads_disabled' => 'Tworzenie nowych wątków jest wyłączone w tej kategorii',
+    'updated' => 'Kategoria zaktualizowana|Kategorie zaktualizowane',
+
+];
